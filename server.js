040dc7833f1d6f73d285e10c78c607ff809ac161
kuwar <kuwarsaurav21@gmail.com>
2018-02-07 10:53:12 +0545

    Initial commit

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + "/views/partials");
+app.set('view engine', 'hbs');
+
+app.use('/statics', express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+app.use((req, res, next) => {
+  let now = new Date().toString();
+  let log = `${now}: ${req.method} ${req.url}`;
+  
+  fs.appendFile('server.log', log + '\n', (error) => {
+    if (error) {
+      console.log('Unable to log in server.log');
+    }
+  });
+  console.log(log);
+  
+  next();
+})
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    welcomeMessage: "Welcome to brand new my homepage",
+    pageTitle: "Home Page"
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render("about.hbs", {
+    pageTitle: "About Page"
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: "Unable to handle the request"
+  });
+});
+app.listen(3000);
